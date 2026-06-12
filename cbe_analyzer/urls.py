81a58f24e34bc68dccab8b339591e72ec1fc41fc
Richard Mweresa <munric2697@gmail.com>
2026-06-12 12:38:48 +0300

    updated dashboard UI and fixed navigation menu

--- cbe_analyzer/urls.py
+++ cbe_analyzer/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from learners import views
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
 
     path('', include('schools.urls')),
+    
+    path('', views.home, name='home'), 
 
     path('learners/', include('learners.urls')),
 
